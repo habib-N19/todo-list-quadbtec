ca7ec9c977eee4546a022d0652f5367534d6fa8f
Habibur <habiburnabiarafat@gmail.com>
2024-06-06 18:23:24 +0600

    added a badge

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,14 +1,20 @@
 'use client'
 import React from 'react'
-import { Card, CardFooter, CardTitle } from './ui/card'
+import { Card, CardContent, CardFooter, CardTitle } from './ui/card'
 import { Button } from './ui/button'
 import { CheckCheckIcon, DeleteIcon, Edit, PlayCircleIcon } from 'lucide-react';
 import { TTaskCardProps } from '@/types';
+import { Badge } from './ui/badge';
 
 export default function TaskCard({ task, handleMarkRunning, handleEdit, handleDelete, handleMarkAsDone, }: TTaskCardProps) {
     return (
         <Card className='p-2'>
-            <CardTitle>{task.task}</CardTitle>
+            <CardTitle className='text-right'>
+                <Badge variant={task.running ? 'secondary' : task.completed ? 'outline' : 'default'}>
+                    {task.running ? 'Running' : task.completed ? 'Done' : 'Pending'}
+                </Badge>
+            </CardTitle>
+            <CardContent>{task.task}</CardContent>
             <CardFooter className='flex justify-between items-center gap-2'>
                 <Button onClick={() => handleMarkRunning(task.id)} variant='outline'><PlayCircleIcon /></Button>
                 <Button onClick={() => handleEdit(task.id)} variant='outline'><Edit></Edit></Button>
